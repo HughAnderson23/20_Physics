91b4170fce449e83ce07e9a920c6f1c80fb583f2
Hugh Anderson <144385898+HughAnderson23@users.noreply.github.com>
2024-11-06 17:17:13 -0500

    added physics to sphere and floor

--- src/script.js
+++ src/script.js
@@ -33,6 +33,32 @@ const environmentMapTexture = cubeTextureLoader.load([
 ])
 
 /**
+ * Physics
+ */
+// World
+const world = new CANNON.World()
+world.gravity.set(0, -9.82, 0)
+
+// Sphere
+const sphereShape = new CANNON.Sphere(0.5)
+const sphereBody = new CANNON.Body({
+    mass: 1,
+    position: new CANNON.Vec3(0, 3, 0),
+    shape: sphereShape
+})
+world.addBody(sphereBody)
+
+// Floor
+const floorShape = new CANNON.Plane()
+const floorBody = new CANNON.Body()
+floorBody.mass = 0
+floorBody.addShape(floorShape)
+floorBody.quaternion.setFromAxisAngle(
+    new CANNON.Vec3(-1, 0, 0), 
+    Math.PI * 0.5)
+world.addBody(floorBody)
+
+/**
  * Test sphere
  */
 const sphere = new THREE.Mesh(
@@ -132,10 +158,19 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  * Animate
  */
 const clock = new THREE.Clock()
+let oldElapsedTime = 0
 
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+    const deltaTime = elapsedTime - oldElapsedTime
+    oldElapsedTime = elapsedTime
+
+    // Update Physics World
+    world.step(1/60, deltaTime, 3)
+
+    sphere.position.copy(sphereBody.position)
+    
 
     // Update controls
     controls.update()
